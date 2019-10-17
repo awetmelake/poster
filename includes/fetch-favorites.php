e5ff08bc8555c4a7c62320a066cae5a9e5a24fe6
awet melake <awetmelake@gmail.com>
2019-10-16 22:31:29 -0500

    fetch favorites as 1d array of user favorites

--- includes/fetch-favorites.php
+++ includes/fetch-favorites.php
@@ -4,7 +4,7 @@ include("./config/db_connect.php");
 
 if (isset($_SESSION['userId'])) {
     $userId = intval($_SESSION['userId']);
-    $sql = "SELECT * FROM favorite_posts WHERE user_id = ? ";
+    $sql = "SELECT post_id FROM favorite_posts WHERE user_id = ? ";
     $stmt = mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt, $sql)) {
         header("Location: index.php?err=sqlerror");
@@ -14,7 +14,6 @@ if (isset($_SESSION['userId'])) {
         mysqli_stmt_execute($stmt);
         $result =  mysqli_stmt_get_result($stmt);
         $favorites = array();
-        ;
         while ($row = mysqli_fetch_assoc($result)) {
             foreach ($row as $r) {
                 array_push($favorites, $r);
