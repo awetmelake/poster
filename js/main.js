8c76664ba927b9c2beab8a7ac7fd10f5ec52c718
awet melake <awetmelake@gmail.com>
2019-10-19 14:51:36 -0500

    resetrefineresults -> postr.awetech instead of localhost

--- js/main.js
+++ js/main.js
@@ -9,5 +9,5 @@ function resetRefineResults() {
   let optionsForm = document.querySelector(".refine-results-form");
   optionsForm.reset();
   console.log("reset");
-  window.location.replace("http://localhost:8000/project/index.php");
+  window.location.replace("https://postr.awettech.com/");
 }
