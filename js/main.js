ecb411d0878f1e4d8a34e8f18e8b22ca094de9e9
awet melake <awetmelake@gmail.com>
2019-10-19 15:13:30 -0500

    material sidenav init

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-
 document.addEventListener("DOMContentLoaded", function() {
   var elems = document.querySelectorAll("select");
   var instances = M.FormSelect.init(elems);
+
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
 
 });
 
