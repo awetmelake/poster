c74ab6069a0957eaf023ff6d3c8458a27f5098b5
awet melake <awetmelake@gmail.com>
2019-10-19 23:28:35 -0500

    shortened query params

--- refine-results.php
+++ refine-results.php
@@ -1,5 +1,5 @@
 <?php
-include("./includes/fetch-results.php");
+include($_SERVER['DOCUMENT_ROOT'] . "/includes/fetch-results.inc.php");
 ?>
 
 <div class="refine-results p-1 left">
@@ -8,25 +8,25 @@ include("./includes/fetch-results.php");
 		<div class="divider"></div><br>
 
 		<label>Location</label>
-		<select name="location" >
+		<select name="l" >
       <option value=""  selected>Select location</option>
 			<?php foreach ($locationOptions as $location): ?>
-				<option <?php echo isset($_GET['location']) && $_GET['location'] == $location ? "selected" : null ?>><?php echo htmlspecialchars($location) ?></option><br>
+				<option <?php echo isset($_GET['l']) && $_GET['l'] == $location ? "selected" : null ?>><?php echo htmlspecialchars($location) ?></option><br>
 			<?php endforeach; ?>
 		</select><br>
 
 		<label>Job Type</label>
 
-    <?php if (isset($_GET['type']) && $_GET['type'] != '') : ?>
-      <div class="">
-        <?php echo htmlspecialchars($_GET['type']); ?>
+    <?php if (isset($_GET['jt']) && $_GET['jt'] != '') : ?>
+      <div >
+        <?php echo htmlspecialchars($_GET['jt']); ?>
       </div>
     <?php else: ?>
-      <div name="type" >
+      <div>
         <?php foreach ($typeOptions as $type): ?>
           <p>
             <label>
-              <input name="type" type="radio" value="<?php echo htmlspecialchars($type) ?>" />
+              <input name="jt" type="radio" value="<?php echo htmlspecialchars($type) ?>" />
               <span><?php echo htmlspecialchars($type) ?></span>
             </label>
           </p>
@@ -35,24 +35,24 @@ include("./includes/fetch-results.php");
     <?php endif; ?>
 
 		<label>Salary estimate</label>
-		<select name="salary_min" >
+		<select name="s" >
       <option value=""  selected>Select min salary</option>
 			<?php foreach ($salaryOptions as $salary) :?>
-					<option <?php echo isset($_GET['salary_min']) && $_GET['salary_min'] == $salary ? "selected" : null ?> value="<?php echo htmlspecialchars($salary) ?>"><?php echo htmlspecialchars($salary) ?></option><br>
+					<option <?php echo isset($_GET['s']) && $_GET['s'] == $salary ? "selected" : null ?> value="<?php echo htmlspecialchars($salary) ?>"><?php echo htmlspecialchars($salary) ?></option><br>
 			<?php endforeach ?>
 		</select><br>
 
 		<label >Sort by</label>
 		<div class="input-field">
-			<select  name="ordered">
+			<select  name="o">
 				<?php foreach ($orderedOptions as $option): ?>
-							<option <?php echo isset($_GET['ordered']) && $_GET['ordered'] == $option ? "selected" : null ?>   value="<?php echo $option ?>"><?php echo $option ?></option>
+							<option <?php echo isset($_GET['o']) && $_GET['o'] == $option ? "selected" : null ?>   value="<?php echo $option ?>"><?php echo $option ?></option>
 					<?php endforeach; ?>
 				</select>
 			</div>
 		<div class="refine-results-btns ">
-			<button class="btn-small green z-depth-0" type="submit">Apply</button>
-			<button class="btn-small red z-depth-0" type="button" onclick="resetRefineResults()">Reset</button>
+			<button class="btn green darken-2 z-depth-0" type="submit">Apply</button>
+			<button class="btn red darken-2 z-depth-0" type="button" onclick="resetRefineResults()">Reset</button>
 		</div>
 	</form>
 </div>
