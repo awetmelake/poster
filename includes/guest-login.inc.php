ffb5305cfe40a5fa95adc26ed9a2fc446552d194
awet melake <awetmelake@gmail.com>
2019-10-23 11:24:25 -0500

    intial commit

---
+++ includes/guest-login.inc.php
@@ -0,0 +1,38 @@
+<?php
+include($_SERVER['DOCUMENT_ROOT'] . '/templates/header.php');
+include($_SERVER['DOCUMENT_ROOT'] . '/config/db_connect.php');
+
+if (isset($_POST['guest-signin-submit'])) {
+    $email = 'guest@guest.com';
+    $password = "guest123";
+
+    if (empty($email) || empty($password)) {
+        echo("<script>location.href = '/signin.php?err=Empty fields';</script>");
+        exit();
+    } else {
+        $sql = "SELECT * FROM users WHERE email=?";
+        $stmt = mysqli_stmt_init($conn);
+
+        if (!mysqli_stmt_prepare($stmt, $sql)) {
+            echo("<script>location.href = '/index.php?err=sqlerror';</script>");
+            exit();
+        } else {
+            mysqli_stmt_bind_param($stmt, 's', $email);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_bind_result($stmt, $userId, $userEmail, $userPassword, $col4, $col5);
+            if (mysqli_stmt_fetch($stmt)) {
+                $passwordCheck = password_verify($password, $userPassword);
+                if ($passwordCheck == false) {
+                    echo("<script>location.href = '/signin.php?err=Incorrect password';</script>");
+                    echo $password . " " .$userPassword;
+                    exit();
+                } elseif ($passwordCheck == true) {
+                    $_SESSION['userId'] = $userId;
+                    echo("<script>location.href = '/index.php?signin=success';</script>");
+                }
+            } else {
+                echo("<script>location.href = '/signin.php?err=User does not exist';</script>");
+            }
+        }
+    }
+}//end POST check
