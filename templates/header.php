58d3d50ba45b29b8db1424328e0d9f02f754889f
awet melake <awetmelake@gmail.com>
2019-10-26 18:47:58 -0500

    material icons instead of text + titles on buttons

--- templates/header.php
+++ templates/header.php
@@ -11,21 +11,22 @@ session_start();
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <!-- material icons -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="/css/main.css">
 </head>
 <body class="grey lighten-3">
   <nav class="white z-depth-0">
     <div class="container">
-      <a href="index.php" class="brand-logo brand-text">Postr</a>
+      <a href="/index.php" class="brand-logo brand-text">Postr</a>
       <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons black-text">menu</i></a>
 
+      <!-- main nav -->
       <ul class="right hide-on-med-and-down">
         <?php if (isset($_SESSION['userId'])) :?>
           <!-- post btn -->
-          <li><a href="add.php" class="btn brand z-depth-0">Post</a></li>
+          <li><a href="/add.php" class="btn brand z-depth-0" title="Create a post"><i class="material-icons">post_add</i></a></li>
 
           <!-- profile btn -->
-          <li><a class="btn brand z-depth-0"  href="profile.php?id=<?php echo htmlspecialchars($_SESSION['userId'])?>">Profile</a></li>
+          <li><a class="btn brand z-depth-0"  href="/profile.php?id=<?php echo htmlspecialchars($_SESSION['userId'])?>" title="Go to user profile"><i class="material-icons">perm_identity</i></a></li>
 
           <!-- logout btn -->
           <li>
@@ -36,20 +37,21 @@ session_start();
 
         <?php else : ?>
           <!-- sign in btn -->
-          <li><a href="signin.php" class="btn brand z-depth-0">Sign in</a></li>
+          <li><a href="/signin.php" class="btn brand z-depth-0">Sign in</a></li>
         <?php endif;?>
       </ul>
 
       </div>
     </nav>
-    <!-- nav -->
+
+    <!-- mobile nav -->
     <ul id="nav-mobile" class="sidenav">
       <?php if (isset($_SESSION['userId'])) :?>
         <!-- post btn -->
-        <li><a href="add.php" class="btn brand z-depth-0">Post</a></li>
+        <li><a href="/add.php" class="btn brand z-depth-0">Post</a></li>
 
         <!-- profile btn -->
-        <li><a class="btn brand z-depth-0"  href="profile.php?id=<?php echo htmlspecialchars($_SESSION['userId'])?>">Profile</a></li>
+        <li><a class="btn brand z-depth-0"  href="/profile.php?id=<?php echo htmlspecialchars($_SESSION['userId'])?>">Profile</a></li>
 
         <!-- logout btn -->
         <li>
@@ -60,7 +62,7 @@ session_start();
 
       <?php else : ?>
         <!-- sign in btn -->
-        <li><a href="signin.php" class="btn brand z-depth-0">Sign in</a></li>
+        <li><a href="/signin.php" class="btn brand z-depth-0">Sign in</a></li>
       <?php endif;?>
     </ul>
 
